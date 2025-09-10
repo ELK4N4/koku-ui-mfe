e0c18360e0310d4490f25f7ba79166d6ffcfc61a
ELK4N4 <ehendler@redhat.com>
2025-09-10 14:44:08 +0300

    remove intl-provider

--- src/fed-modules/mfeOptimizationsWrapper.tsx
+++ src/fed-modules/mfeOptimizationsWrapper.tsx
@@ -1,12 +1,6 @@
-/* eslint-disable no-console */
-import IntlProvider from '@redhat-cloud-services/frontend-components-translations/Provider';
-import { getLocale } from 'components/i18n';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { mfeStore } from 'store';
-
-// eslint-disable-next-line no-restricted-imports
-import messages from '../../locales/data.json';
 
 export interface OptimizationsWrapperOwnProps {
   children?: React.ReactNode;
@@ -15,15 +9,11 @@ export interface OptimizationsWrapperOwnProps {
 type OptimizationsWrapperProps = OptimizationsWrapperOwnProps;
 
 const MfeOptimizationsWrapper: React.FC<OptimizationsWrapperProps> = ({ children }: OptimizationsWrapperOwnProps) => {
-  const locale = getLocale();
-
   // Note: className is a workaround for ConsoleDot outputting the app name instead of module name
   return (
-    <IntlProvider defaultLocale="en" locale={locale} messages={messages[locale]} onError={console.log}>
-      <Provider store={mfeStore as any}>
-        <div className="costManagementMfe">{children}</div>
-      </Provider>
-    </IntlProvider>
+    <Provider store={mfeStore as any}>
+      <div className="costManagementMfe">{children}</div>
+    </Provider>
   );
 };
 
